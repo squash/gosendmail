ed47d26246a2415406ccbe1144d7d9cf2585c679
JG <josh@shortcutsolutions.net>
2016-07-28 14:33:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+gosendmail
+==========
+
+A tiny sendmail replacement for environments with smarthosts, based on [mhsendmail](https://github.com/mailhog/mhsendmail)
+
+
+```bash
+> go get github.com/squash/gosendmail
+
+> gosendmail test@mailhog.local <<EOF
+From: App <app@mlocal>
+To: Test <test@mlocal>
+Subject: Test message
+
+Some content!
+EOF
+```
+
+gosendmail looks for default options in /etc/gosendmail.conf. An example is provided.
+
+You can override the from address (for SMTP `MAIL FROM`):
+
+```bash
+./gosendmail --from="admin@mailhog.local" test@mailhog.local ...
+```
+
+Or pass in multiple recipients:
+
+```bash
+./gosendmail --from="admin@ocal" test@mailhog.local test2@mailhog.local ...
+```
+
+Or override the destination SMTP server:
+
+```bash
+./gosendmail --smtp-addr="localhost:1026" test@local ...
+```
+
+To use from php.ini
+
+```
+sendmail_path = /usr/local/bin/gosendmail
+```
+
+### Licence
+
+Original Copyright ©‎ 2015 - 2016, Ian Kent (http://iankent.uk)
+(c) 2016 Josh Grebe (https://github.com/squash/)
+
+Released under MIT license, see [LICENSE](LICENSE.md) for details.
---
+++ gosendmail.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/mail"
+	"net/smtp"
+	"os"
+	"os/user"
+	"github.com/spf13/viper"
+	"github.com/spf13/pflag"
+)
+
+// Go runs the MailHog sendmail replacement.
+func main() {
+
+	host, err := os.Hostname()
+	if err != nil {
+		host = "localhost"
+	}
+
+	username := "nobody"
+	user, err := user.Current()
+	if err == nil && user != nil && len(user.Username) > 0 {
+		username = user.Username
+	}
+
+	fromaddr := username + "@" + host
+	viper.SetDefault("fromaddr", fromaddr)
+	viper.SetDefault("smtpaddr", "localhost:25")
+	viper.SetDefault("logfile", "")
+	viper.SetConfigName("gosendmail")
+	viper.AddConfigPath("/etc")
+	viper.ReadInConfig()
+	var Log *log.Logger
+	logfile:=viper.GetString("logfile")
+	if logfile != "" {
+		file, err := os.OpenFile(logfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+		    log.Fatalln("Failed to open log file:", err)
+		}		
+
+		Log = log.New(file,
+		    "Info: ",
+		    log.Ldate|log.Ltime|log.Lshortfile)
+
+	} else {
+			Log=log.New(os.Stderr, "Info:", log.Ldate|log.Ltime|log.Lshortfile)
+			
+	}
+	
+	var recip []string
+
+    smtpaddr:=viper.GetString("smtpaddr")
+    fromaddr=viper.GetString("fromaddr")
+	// override defaults from cli flags
+	pflag.StringVar(&smtpaddr, "smtp-addr", smtpaddr, "SMTP server address")
+	pflag.StringVarP(&fromaddr, "from", "f", fromaddr, "SMTP sender")
+	pflag.BoolP("long-i", "i", true, "Ignored. This flag exists for sendmail compatibility.")
+	pflag.BoolP("long-t", "t", true, "Ignored. This flag exists for sendmail compatibility.")
+	pflag.Parse()
+
+	// allow recipient to be passed as an argument
+	recip = pflag.Args()
+
+	Log.Println("Starting: From ", fromaddr, " To ", recip)
+
+	body, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		Log.Fatal( "error reading stdin")
+	}
+
+	msg, err := mail.ReadMessage(bytes.NewReader(body))
+	if err != nil {
+		Log.Fatal("error parsing message body")
+	}
+
+	if len(recip) == 0 {
+		// We only need to parse the message to get a recipient if none where
+		// provided on the command line.
+		recip = append(recip, msg.Header.Get("To"))
+	}
+
+	err = smtp.SendMail(smtpaddr, nil, fromaddr, recip, body)
+	if err != nil {
+		log.Fatal("Error sending mail: ", err)
+	}
+
+}
